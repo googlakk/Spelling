176e0d3a8d8588d223d426f7217eacfb8374912c
Emir <googl.akk@mail.ru>
2023-03-19 01:05:52 +0300

    fix audio2

--- src/utils/sounds.js
+++ src/utils/sounds.js
@@ -2,6 +2,6 @@ const importAllAudio = (r) => {
   return r.keys().map(r);
 }
 
-const sounds = importAllAudio(require.context('../assets/audio/', false, /.wav/ || / .wav/));
+const sounds = importAllAudio(require.context('../assets/audio/', false, /.mp3/ || / .mp3/));
 
 export default sounds;
